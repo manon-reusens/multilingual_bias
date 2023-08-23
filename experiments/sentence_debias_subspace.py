331faf1669d42346c4e00c41be9d791af44abf6f
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-08-23 14:42:26 +0200

    Update sentence_debias_subspace.py

--- experiments/sentence_debias_subspace.py
+++ experiments/sentence_debias_subspace.py
@@ -58,7 +58,7 @@ parser.add_argument(
     action="store",
     type=str,
     default='en',
-    choices=["nl", "en", "de", "fr"],
+    choices=["nl", "en", "de", "fr","pl","ru"],
     required=True,
     help="The language the bias is mitigated in.",
 )
