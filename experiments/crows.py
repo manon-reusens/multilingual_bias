2b0f2c36fe78ddbf92cd252226c32d4930920909
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-08-23 15:26:49 +0200

    Update crows.py

--- experiments/crows.py
+++ experiments/crows.py
@@ -72,7 +72,7 @@ parser.add_argument(
     action="store",
     type=str,
     default='en',
-    choices=['en', 'fr', 'nl', 'de','pl','ru' ],
+    choices=['en', 'fr', 'nl', 'de','pl','ru','ca' ],
     help="Language to evaluate on.",
 )
 
@@ -134,6 +134,11 @@ if __name__ == "__main__":
             input=f"{args.persistent_dir}/data/crows/crows_pairs_anonymized_ru.csv"
         else:
             input=f"{args.persistent_dir}/data/crows/crows_pairs_anonymized_ru_s{str(args.seed)}.csv"
+    elif args.lang == 'ca':
+        if args.seed==None:
+            input=f"{args.persistent_dir}/data/crows/crows_pairs_anonymized_ca.csv"
+        else:
+            input=f"{args.persistent_dir}/data/crows/crows_pairs_anonymized_ca_s{str(args.seed)}.csv"
 
     runner = CrowSPairsRunner(
         model=model,
